9124eea81530cd57b122d2d3c035d73f71a86bde
Hackodezo <athul@hackodezo.com>
2018-07-20 19:29:25 +0530

    small changes

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoListItem = ({video, onVideoSelect}) =>{
   const imageUrl = video.snippet.thumbnails.default.url;
-  const title =  video.snippet.title
+  const title =  video.snippet.title;
   return (
     <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
@@ -17,6 +17,6 @@ const VideoListItem = ({video, onVideoSelect}) =>{
       </div>
     </li>
   );
-}
+};
 
 export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ class App extends Component{
       videos: [],
       selectedVideo: null
     };
-    this.videoSearch("New Hindi Songs")
+    this.videoSearch("New Malayalam Songs")
   }
 
   videoSearch(term){
@@ -26,7 +26,7 @@ class App extends Component{
     })
   }
   render(){
-    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (
       <div>
         <SearchBar onSearchTermChange={videoSearch}/>
